311252cd612ca7850c97c33e712cf854cd1bada7
Valdimir Ladynev <v.ladynev@gmail.com>
2017-06-13 12:02:29 +0300

    menu api: updated JsonUtils

--- src/main/java/com/smartling/connector/yext/sdk/utils/JsonUtils.java
+++ src/main/java/com/smartling/connector/yext/sdk/utils/JsonUtils.java
@@ -6,25 +6,35 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 import java.net.URL;
 
-public final class JsonUtils {
+public final class JsonUtils
+{
 
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
-    private JsonUtils() {
+    static
+    {
+        MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    private JsonUtils()
+    {
 
     }
 
-    public static <T> String toJsonString(T object) {
-        try {
-            MAPPER.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-            MAPPER.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    public static <T> String toJsonString(T object)
+    {
+        try
+        {
             return object == null ? null : MAPPER.writeValueAsString(object);
-        } catch (Exception ex) {
+        } catch (Exception ex)
+        {
             throw ExceptionUtils.toRuntimeException(ex);
         }
     }
 
-    public static <T> T fromJsonByClassLoader(String classLoaderResource, Class<T> type) {
+    public static <T> T fromJsonByClassLoader(String classLoaderResource, Class<T> type)
+    {
         return fromJson(
                 Thread.currentThread().getContextClassLoader().getResource(classLoaderResource),
                 type
@@ -32,10 +42,13 @@ public final class JsonUtils {
     }
 
 
-    public static <T> T fromJson(URL urlToJson, Class<T> type) {
-        try {
+    public static <T> T fromJson(URL urlToJson, Class<T> type)
+    {
+        try
+        {
             return MAPPER.readValue(urlToJson, type);
-        } catch (Exception ex) {
+        } catch (Exception ex)
+        {
             throw ExceptionUtils.toRuntimeException(ex);
         }
     }
