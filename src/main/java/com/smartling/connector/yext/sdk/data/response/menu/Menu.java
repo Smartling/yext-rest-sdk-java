15e1d12d9ab9e08433b8766c3a5232c2bc3d7727
Valdimir Ladynev <v.ladynev@gmail.com>
2017-07-04 10:26:42 +0300

    removed swagger

--- src/main/java/com/smartling/connector/yext/sdk/data/response/menu/Menu.java
+++ src/main/java/com/smartling/connector/yext/sdk/data/response/menu/Menu.java
@@ -1,39 +1,26 @@
 package com.smartling.connector.yext.sdk.data.response.menu;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.List;
 
-@ApiModel(description = "A menu with sections")
 public class Menu
 {
 
-    @ApiModelProperty(value = "List name.")
     private String name;
 
-    @ApiModelProperty(example = "en", value = "List language in ISO 639-1 format. An ISO 3166-1 alpha-2 country code can optionally be appended to the language.  **Examples:** en, en_GB, fr_CA ")
     private String language;
 
-    @ApiModelProperty(value = "List title that appears on listings.")
     private String title;
 
-    @ApiModelProperty(value = "Indicates whether the List should be shown on your listings.")
     private Boolean publish;
 
-    @ApiModelProperty(value = "ID of account that owns this List.")
     private String accountId;
 
-    @ApiModelProperty(value = "List ID.")
     private String id;
 
-    @ApiModelProperty(value = "Number of items in the List.")
     private Integer size;
 
-    @ApiModelProperty(example = "USD", value = "The three-letter ISO 4217 currency code. Defaults to USD.")
     private String currency;
 
-    @ApiModelProperty(value = "A list of sections.")
     private List<MenuSection> sections;
 
     public String getName()
